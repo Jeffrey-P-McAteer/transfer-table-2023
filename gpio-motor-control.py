e24822f45194b02dabb7f174800fc1bd6492b92a
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-10-01 13:35:05 -0400

    moved motor to easier test setup

--- gpio-motor-control.py
+++ gpio-motor-control.py
@@ -17,8 +17,9 @@ import sys, tty, termios, time
 motor_enable_pin = 2
 motor_direction_pin = 2
 motor_step_pin = 0
-delay = 0.0001              # By playing with this delay you can influence the rotational speed.
-pulses_per_rev = 8000        # This can be configured on the driver using the DIP-switches
+
+delay = 0.0004              # By playing with this delay you can influence the rotational speed.
+pulses_per_rev = 400        # This can be configured on the driver using the DIP-switches
 GPIO.setup(motor_enable_pin, GPIO.OUT)
 GPIO.setup(motor_direction_pin, GPIO.OUT)
 GPIO.setup(motor_step_pin, GPIO.OUT)
