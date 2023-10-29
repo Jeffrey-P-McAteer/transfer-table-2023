cc34374011fa62ea14490c80d5afb7007e544b81
Jeffrey McAteer <jeffrey@jmcateer.com>
2023-10-29 12:53:23 -0400

    lower max speed

--- gpio-motor-control.c
+++ gpio-motor-control.c
@@ -507,7 +507,7 @@ void step_n_eased(int n, int ramp_up_end_n, DirectionedStepFunc step_func) {
 
   // 1 is as fast we we'll be bothering to measure, 30 is too fast for a begin ramp-up
   int slowest_us = 400;
-  int fastest_us = 42;
+  int fastest_us = 78;
   
   // For very short steps, limit top speed & change ramp up bounds.
   if (n < ramp_up_end_n) {
