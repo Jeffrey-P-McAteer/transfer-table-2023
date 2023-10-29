9854a3b633b98fafbe952a067049704c5cb288e7
Jeffrey McAteer <jeffrey@jmcateer.com>
2023-10-29 11:32:41 -0400

    make push save position

--- gpio-motor-control.c
+++ gpio-motor-control.c
@@ -773,7 +773,7 @@ void perform_keypress(__u16 code) {
     motor_stop_requested = true;
     printf("Motor stop requested! (code=%d)\n", code);
   }
-  else if (code == KEY_BACKSPACE || code == 14 /* backspace */ || code == KEY_EQUAL) {
+  else if (code == KEY_BACKSPACE || code == 14 /* backspace */ || code == KEY_EQUAL || code == 113 /* push */) {
     //printf("Got Backspace, TODO enter assignment for next number input! (code=%d)\n", code);
     if (pmem.position >= 0 && pmem.position < NUM_POSITIONS) {
       pmem.position_data[pmem.position].steps_from_0 = pmem.table_steps_from_0;
@@ -804,10 +804,6 @@ void perform_keypress(__u16 code) {
       }
       table_state = TABLE_STOPPED;
     });
-  }
-  else if (code == 113 /* push */) {
-    motor_stop_requested = true;
-    printf("Dial motor stop requested! (code=%d)\n", code);
   }
   else if (code != 0) {
     printf("Got unknown key, %d!\n", code);
