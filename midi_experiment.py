adf1770506b3518b96a869d78440f1fea4c233f9
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-01-07 16:01:03 -0500

    experiment v1, discovered we cannot turn LEDs on from simple messages

---
+++ midi_experiment.py
@@ -0,0 +1,46 @@
+
+import os
+import sys
+import subprocess
+import traceback
+import time
+
+python_libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.py-env'))
+os.makedirs(python_libs_dir, exist_ok=True)
+sys.path.append(python_libs_dir)
+
+try:
+  import mido
+except:
+  subprocess.run([
+    sys.executable, '-m', 'pip', 'install', f'--target={python_libs_dir}', 'mido[ports-rtmidi]'
+  ])
+  import mido
+
+
+# aseqdump -p 'WORLDE'
+
+print(f'Input names = {mido.get_input_names()}')
+print(f'Output names = {mido.get_output_names()}')
+
+WORLDE_out_name = [x for x in mido.get_output_names() if 'worlde' in x.lower()][0]
+print(f'WORLDE_out_name = {WORLDE_out_name}')
+
+outport = mido.open_output(WORLDE_out_name)
+
+msg = mido.Message('note_on', note=40)
+outport.send(msg)
+
+time.sleep(1.2)
+
+msg = mido.Message('note_off', note=40)
+outport.send(msg)
+
+print('Printing events recieved...')
+with mido.open_input(WORLDE_out_name) as inport:
+  for msg in inport:
+    print(f'msg = {msg}')
+
+
+
+
