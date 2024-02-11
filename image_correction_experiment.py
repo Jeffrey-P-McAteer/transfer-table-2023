a5075c8847a9748ee59549a398897e7b79bcc9dd
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-02-11 15:49:38 -0500

    r+d studd

--- image_correction_experiment.py
+++ image_correction_experiment.py
@@ -6,6 +6,7 @@ import subprocess
 import shutil
 import datetime
 import traceback
+import time
 
 python_libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.py-env'))
 os.makedirs(python_libs_dir, exist_ok=True)
@@ -71,31 +72,10 @@ def main():
         elapsed_time = delta_time.total_seconds()
         cur_fps = numpy.around(frames / elapsed_time, 1)
 
-        # Perform image normalization(s)
-
-        int_a = 50
-        if os.path.exists('/tmp/int_a'):
-          with open('/tmp/int_a', 'r') as fd:
-            try:
-              int_a = int(fd.read().strip())
-            except:
-              traceback.print_exc()
-
-        int_b = 50
-        if os.path.exists('/tmp/int_b'):
-          with open('/tmp/int_b', 'r') as fd:
-            try:
-              int_b = int(fd.read().strip())
-            except:
-              traceback.print_exc()
-
-
-        img = cv2.Canny(img, int_a, int_b)
-
-        # img = cv2.Sobel(src=img, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=15) # Combined X and Y Sobel Edge Detection
+        img = do_track_detection(img, cap_width, cap_height)
 
         # draw FPS text and display image
-        cv2.putText(img, f'FPS: {cur_fps} a: {int_a} b: {int_b}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (128, 128, 128), 2, cv2.LINE_AA)
+        cv2.putText(img, f'FPS: {cur_fps}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (128, 128, 128), 2, cv2.LINE_AA)
         cv2.imshow("webcam", img)
 
         # wait 1ms for ESC to be pressed
@@ -103,7 +83,23 @@ def main():
         if (key == 27):
             break
   else:
-    print(f'TODO open {image_file}')
+    print(f'Opening {image_file}')
+
+    orig_img = cv2.imread(image_file, 0)
+    height, width = orig_img.shape
+
+    while True:
+
+      img = do_track_detection(orig_img, width, height)
+
+      cv2.imshow("webcam", img)
+
+      # wait 1ms for ESC to be pressed
+      key = cv2.waitKey(1)
+      if (key == 27):
+          break
+
+      time.sleep(0.05)
 
   # release resources
   try:
@@ -112,6 +108,31 @@ def main():
   except:
     pass
 
+
+def do_track_detection(img, width, height):
+
+  int_a = 50
+  if os.path.exists('/tmp/int_a'):
+    with open('/tmp/int_a', 'r') as fd:
+      try:
+        int_a = int(fd.read().strip())
+      except:
+        traceback.print_exc()
+
+  int_b = 90
+  if os.path.exists('/tmp/int_b'):
+    with open('/tmp/int_b', 'r') as fd:
+      try:
+        int_b = int(fd.read().strip())
+      except:
+        traceback.print_exc()
+
+
+  img = cv2.Canny(img, int_a, int_b)
+
+  cv2.putText(img, f'a: {int_a} b: {int_b}', (10, height-30), cv2.FONT_HERSHEY_SIMPLEX, 1, (128, 128, 128), 2, cv2.LINE_AA)
+
+  return img
 
 
 
