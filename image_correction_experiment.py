f008139b7327b563db49957d5ad985bc52b15008
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-02-11 15:43:02 -0500

    single-image testing

--- image_correction_experiment.py
+++ image_correction_experiment.py
@@ -30,72 +30,87 @@ except:
 
 
 def main():
+  video_device_num = 0
+  image_file = None
+  try:
+    if len(sys.argv) > 1:
+      video_device_num = int(sys.argv[1])
+  except:
+    if len(sys.argv) > 1:
+      image_file = sys.argv[1]
+
   # From https://stackoverflow.com/questions/62576326/python3-process-and-display-webcam-stream-at-the-webcams-fps
   # create display window
   cv2.namedWindow("floatme", cv2.WINDOW_NORMAL)
 
-  # initialize webcam capture object
-  cap = cv2.VideoCapture(0)
+  if image_file is None:
+    # initialize webcam capture object
+    cap = cv2.VideoCapture(video_device_num)
 
-  # retrieve properties of the capture object
-  cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-  cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-  cap_fps = cap.get(cv2.CAP_PROP_FPS)
-  fps_sleep = int(1000 / cap_fps)
-  print('* Capture width:', cap_width)
-  print('* Capture height:', cap_height)
-  print('* Capture FPS:', cap_fps, 'ideal wait time between frames:', fps_sleep, 'ms')
+    # retrieve properties of the capture object
+    cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    cap_fps = cap.get(cv2.CAP_PROP_FPS)
+    fps_sleep = int(1000 / cap_fps)
+    print('* Capture width:', cap_width)
+    print('* Capture height:', cap_height)
+    print('* Capture FPS:', cap_fps, 'ideal wait time between frames:', fps_sleep, 'ms')
 
-  # initialize time and frame count variables
-  last_time = datetime.datetime.now()
-  frames = 0
+    # initialize time and frame count variables
+    last_time = datetime.datetime.now()
+    frames = 0
 
-  # main loop: retrieves and displays a frame from the camera
-  while True:
-      # blocks until the entire frame is read
-      success, img = cap.read()
-      frames += 1
+    # main loop: retrieves and displays a frame from the camera
+    while True:
+        # blocks until the entire frame is read
+        success, img = cap.read()
+        frames += 1
 
-      # compute fps: current_time - last_time
-      delta_time = datetime.datetime.now() - last_time
-      elapsed_time = delta_time.total_seconds()
-      cur_fps = numpy.around(frames / elapsed_time, 1)
+        # compute fps: current_time - last_time
+        delta_time = datetime.datetime.now() - last_time
+        elapsed_time = delta_time.total_seconds()
+        cur_fps = numpy.around(frames / elapsed_time, 1)
 
-      # Perform image normalization(s)
+        # Perform image normalization(s)
 
-      int_a = 50
-      if os.path.exists('/tmp/int_a'):
-        with open('/tmp/int_a', 'r') as fd:
-          try:
-            int_a = int(fd.read().strip())
-          except:
-            traceback.print_exc()
+        int_a = 50
+        if os.path.exists('/tmp/int_a'):
+          with open('/tmp/int_a', 'r') as fd:
+            try:
+              int_a = int(fd.read().strip())
+            except:
+              traceback.print_exc()
 
-      int_b = 50
-      if os.path.exists('/tmp/int_b'):
-        with open('/tmp/int_b', 'r') as fd:
-          try:
-            int_b = int(fd.read().strip())
-          except:
-            traceback.print_exc()
+        int_b = 50
+        if os.path.exists('/tmp/int_b'):
+          with open('/tmp/int_b', 'r') as fd:
+            try:
+              int_b = int(fd.read().strip())
+            except:
+              traceback.print_exc()
 
 
-      img = cv2.Canny(img, int_a, int_b)
+        img = cv2.Canny(img, int_a, int_b)
 
-      # img = cv2.Sobel(src=img, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=15) # Combined X and Y Sobel Edge Detection
+        # img = cv2.Sobel(src=img, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=15) # Combined X and Y Sobel Edge Detection
 
-      # draw FPS text and display image
-      cv2.putText(img, f'FPS: {cur_fps} a: {int_a} b: {int_b}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (128, 128, 128), 2, cv2.LINE_AA)
-      cv2.imshow("webcam", img)
+        # draw FPS text and display image
+        cv2.putText(img, f'FPS: {cur_fps} a: {int_a} b: {int_b}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (128, 128, 128), 2, cv2.LINE_AA)
+        cv2.imshow("webcam", img)
 
-      # wait 1ms for ESC to be pressed
-      key = cv2.waitKey(1)
-      if (key == 27):
-          break
+        # wait 1ms for ESC to be pressed
+        key = cv2.waitKey(1)
+        if (key == 27):
+            break
+  else:
+    print(f'TODO open {image_file}')
 
   # release resources
-  cv2.destroyAllWindows()
-  cap.release()
+  try:
+    cv2.destroyAllWindows()
+    cap.release()
+  except:
+    pass
 
 
 
