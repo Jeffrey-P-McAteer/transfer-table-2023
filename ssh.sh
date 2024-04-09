2f52ae4ef70072b4198d776045a32cea6abf132f
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-04-09 17:47:49 -0400

    notes, docs on b8:27:eb:ce:37:8f

--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "$pi_ip" ]] ; then
-  #pi_ip=$(lanipof 'b8:27:eb:ce:37:8f')
+  #pi_ip=$(lanipof 'b8:27:eb:ce:37:8f') # wlan0, internal. Still a bit good!
   pi_ip=$(lanipof '6c:5a:b0:38:d2:b6')
   echo "Pi IP = $pi_ip"
 fi
