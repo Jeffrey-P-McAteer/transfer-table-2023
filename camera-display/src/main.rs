ccf7913f57ddce4311f3b4a5f6841373e16c3836
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-04-13 09:50:58 -0400

    dumb experiment time

--- camera-display/src/main.rs
+++ camera-display/src/main.rs
@@ -1,5 +1,5 @@
 
-use embedded_graphics_framebuf::FrameBuf;
+
 
 use v4l::buffer::Type;
 use v4l::io::mmap::Stream;
@@ -13,15 +13,26 @@ const GPIO_MOTOR_KEYS_IN_DIR: &'static str = "/tmp/gpio_motor_keys_in";
 
 
 fn main() {
+  loop {
+    if let Err(e) = do_camera_loop() {
+      println!("[ do_camera_loop exited ] {:?}", e);
+    }
+    std::thread::sleep(std::time::Duration::from_millis(1200));
+  }
+}
 
-  let mut dev = Device::new(0).expect("Failed to open device");
+
+// Ought to run infinitely, returns result so we can handle hardware disconnect & re-connects
+#[allow(unreachable_code)]
+fn do_camera_loop() -> Result<(), Box<dyn std::error::Error>> {
+  let mut dev = Device::new(0)?;
 
   // Let's say we want to explicitly request another format
-  let mut fmt = dev.format().expect("Failed to read format");
+  let mut fmt = dev.format()?;
   fmt.width = 1280;
   fmt.height = 720;
   fmt.fourcc = FourCC::new(b"YUYV");
-  let fmt = dev.set_format(&fmt).expect("Failed to write format");
+  let fmt = dev.set_format(&fmt)?;
 
   // The actual format chosen by the device driver may differ from what we
   // requested! Print it out to get an idea of what is actually used now.
@@ -41,15 +52,37 @@ fn main() {
 
   // Create the stream, which will internally 'allocate' (as in map) the
   // number of requested buffers for us.
-  let mut stream = Stream::with_buffers(&mut dev, Type::VideoCapture, 4)
-      .expect("Failed to create buffer stream");
+  let mut stream = Stream::with_buffers(&mut dev, Type::VideoCapture, 4)?;
 
   // At this point, the stream is ready and all buffers are setup.
   // We can now read frames (represented as buffers) by iterating through
   // the stream. Once an error condition occurs, the iterator will return
   // None.
+
+  // Open framebuffer for output
+  let fb = match linuxfb::Framebuffer::new("/dev/fb0") {
+    Ok(f) => f,
+    Err(e) => {
+        println!("linuxfb::Framebuffer::new() e = {:?}", e);
+        return Ok(());
+    }
+  };
+
+  println!("FB Size in pixels: {:?}", fb.get_size());
+
+  println!("FB Bytes per pixel: {:?}", fb.get_bytes_per_pixel());
+
+  println!("FB Physical size in mm: {:?}", fb.get_physical_size());
+
+
+  let mut loop_i = 0;
   loop {
-      let (buf, meta) = stream.next().unwrap();
+      loop_i += 1;
+      if loop_i > 1000 {
+        loop_i = 0;
+      }
+
+      let (buf, meta) = stream.next()?;
       println!(
           "Buffer size: {}, seq: {}, timestamp: {}",
           buf.len(),
@@ -62,9 +95,41 @@ fn main() {
       // copy it. This is a best-effort tradeoff solution that allows for
       // zero-copy readers while enforcing a full clone of the data for
       // writers.
+
+      let mut data = match fb.map() {
+        Ok(m) => m,
+        Err(e) => {
+          println!("fb.map() e = {:?}", e);
+          return Ok(());
+        }
+      };
+
+      // Make everything black:
+      for i in 0..data.len() {
+        data[i] = 0;
+      }
+
+      std::thread::sleep(std::time::Duration::from_millis(50));
+
+      // Make everything white:
+      for i in 0..data.len() {
+        data[i] = 0xFF;
+      }
+
+      if loop_i > 50 {
+        break;
+      }
+
   }
 
-
+  Ok(())
 }
+
+
+
+
+
+
+
 
 
