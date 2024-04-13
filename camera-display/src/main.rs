748eadf421f25573ab2146230ff90bd0b1a563ac
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-04-13 11:19:24 -0400

    output pixels v0!

--- camera-display/src/main.rs
+++ camera-display/src/main.rs
@@ -31,13 +31,16 @@ fn do_camera_loop() -> Result<(), Box<dyn std::error::Error>> {
   let mut fmt = dev.format()?;
   fmt.width = 640;
   fmt.height = 480;
-  fmt.fourcc = FourCC::new(b"YUYV");
+  fmt.fourcc = FourCC::new(b"YUYV"); // YCbCr 4:2:2 pixels
   // fmt.fourcc = FourCC::new(b"MJPG"); // Slow!
   let fmt = dev.set_format(&fmt)?;
 
   // The actual format chosen by the device driver may differ from what we
   // requested! Print it out to get an idea of what is actually used now.
-  println!("Format in use:\n{}", fmt);
+  println!("Camera Image Format in use:\n{}", fmt);
+
+  let img_fmt_h = fmt.height as usize;
+  let img_fmt_w = fmt.width as usize;
 
   // Now we'd like to capture some frames!
   // First, we need to create a stream to read buffers from. We choose a
@@ -60,8 +63,17 @@ fn do_camera_loop() -> Result<(), Box<dyn std::error::Error>> {
   // the stream. Once an error condition occurs, the iterator will return
   // None.
 
+  let devices = linuxfb::Framebuffer::list()?;
+  if devices.len() < 1 {
+    println!("No Framebufffer devices found, exiting!");
+    return Ok(());
+  }
+
+  let fb_device = devices[0].clone();
+  println!("Using fb_device = {:?}", fb_device);
+
   // Open framebuffer for output
-  let fb = match linuxfb::Framebuffer::new("/dev/fb0") {
+  let fb = match linuxfb::Framebuffer::new(fb_device) {
     Ok(f) => f,
     Err(e) => {
         println!("linuxfb::Framebuffer::new() e = {:?}", e);
@@ -70,11 +82,15 @@ fn do_camera_loop() -> Result<(), Box<dyn std::error::Error>> {
   };
 
   println!("FB Size in pixels: {:?}", fb.get_size());
+  let fb_bpp = fb.get_bytes_per_pixel() as usize;
+  println!("FB Bytes per pixel: {:?}", fb_bpp);
+  let fb_pxlyt = fb.get_pixel_layout();
+  println!("FB Pixel layout: {:?}", fb_pxlyt);
 
-  println!("FB Bytes per pixel: {:?}", fb.get_bytes_per_pixel());
-
-  println!("FB Physical size in mm: {:?}", fb.get_physical_size());
-
+  // Ensure screen is on
+  if let Err(e) = fb.blank(linuxfb::BlankingLevel::Unblank) {
+    println!("fb.blank() e = {:?}", e);
+  }
 
   let mut loop_i = 0;
   loop {
@@ -97,7 +113,7 @@ fn do_camera_loop() -> Result<(), Box<dyn std::error::Error>> {
       // zero-copy readers while enforcing a full clone of the data for
       // writers.
 
-      let mut data = match fb.map() {
+      let mut fb_mem = match fb.map() {
         Ok(m) => m,
         Err(e) => {
           println!("fb.map() e = {:?}", e);
@@ -105,17 +121,26 @@ fn do_camera_loop() -> Result<(), Box<dyn std::error::Error>> {
         }
       };
 
-      // Make everything black:
-      for i in 0..data.len() {
-        data[i] = 0;
+      // Decode YCbCr 4:2:2 pixels into BGR pixels and send to framebuffer!
+      for y in 0..img_fmt_h {
+        for x in 0..img_fmt_w {
+          let fb_px_offset = ( ((y*img_fmt_h) + x) * fb_bpp) as usize;
+          if fb_px_offset+fb_bpp >= buf.len() {
+            continue;
+          }
+
+          let r_idx = fb_px_offset + (fb_pxlyt.red.offset / 8) as usize;
+          let g_idx = fb_px_offset + (fb_pxlyt.green.offset / 8) as usize;
+          let b_idx = fb_px_offset + (fb_pxlyt.blue.offset / 8) as usize;
+
+          fb_mem[r_idx] = 0xFF;
+          fb_mem[g_idx] = 0x00;
+          fb_mem[b_idx] = 0x00;
+
+
+        }
       }
 
-      std::thread::sleep(std::time::Duration::from_millis(50));
-
-      // Make everything white:
-      for i in 0..data.len() {
-        data[i] = 0xFF;
-      }
 
       if loop_i > 50 {
         break;
