8a2e079e0d72efd467d8e89cd4d4def84ce0a2a7
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-04-14 20:01:41 -0400

    swapped to the internal MAC

--- ssh.sh
+++ ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ -z "$pi_ip" ]] ; then
-  #pi_ip=$(lanipof 'b8:27:eb:ce:37:8f') # wlan0, internal. Still a bit good!
-  pi_ip=$(lanipof '6c:5a:b0:38:d2:b6')
+  pi_ip=$(lanipof 'b8:27:eb:ce:37:8f') # wlan0, internal. Still a bit good!
+  #pi_ip=$(lanipof '6c:5a:b0:38:d2:b6')
   echo "Pi IP = $pi_ip"
 fi
 
